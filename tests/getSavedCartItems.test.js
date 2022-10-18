c36e53c465d45feda0771a4c5b414ec73dbadcfd
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-10-18 12:52:58 -0300

    requisito 6 e 7 terminados

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,9 +1,17 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const getSavedCartItems = require('../helpers/getSavedCartItems');
+const { expect } = require('expect');
 
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test(`if runs getSavedCartItems function, the method localStorage.getItem has been called.`, () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  test(`if runs getSavedCartItems function, the method localStorage.getItem has been called with cartItems as parameter.`, () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,9 +1,18 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
+const { expect } = require('expect');
 
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test(`if runs saveCartItems function with cartItem as argument, the method localStorage.setItem have been called`, async () => {
+    saveCartItems('cartItem');
+    
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+   test(`if runs saveCartItems function wwith cartItem as argument, the method localStorage.setItem has been called with two parameters`, () => {
+    saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'cartItem');
+   });
 });
